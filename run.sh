479ac57cee1331764ada8e666fe3e38b57ffe0ac
sudo <119590800+0x7375646F@users.noreply.github.com>
2024-11-26 09:35:14 +0545

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 apt update && apt upgrade -y && apt install python3 -y && pip3 install requests
-echo python3 main.py >> ~/.bashrc
+echo ~/Softwarica-Gateway-AutoLogin/python3 main.py >> ~/.bashrc
